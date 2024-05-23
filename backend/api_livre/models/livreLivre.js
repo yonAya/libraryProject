5aeeaf74edfbb8927d47911ab7d6ca8dd87d43a6
rimoelharras <145035693+rimo1803@users.noreply.github.com>
2024-05-23 15:27:12 +0100

    ff

--- backend/api_livre/models/livreLivre.js
+++ backend/api_livre/models/livreLivre.js
@@ -1,5 +1,20 @@
 import { Schema, model } from 'mongoose';
 
-const livreSchema = new Schema({});
+const livreSchema = new Schema({
+    code: {
+      type: String,
+      required: true
+    },
+    titre: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String
+    },
+    auteur: {
+      type: String
+    }
+  });
 
 export default model('Livre', livreSchema);
---
+++ backend/api_livre/routes/livreRoute.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import BookModel from '../models/livreLivre';
+
+const routes = express.Router();
+
+routes.get('/api/v1/livre/:idLivre', async (req, res) => {
+    const { idLivre } = req.params;
+    try {
+        const livre = await BookModel.findById(idLivre);
+        if (livre) {
+            res.status(200).json(livre);
+        } else {
+            res.status(404).json({ message: "Livre non trouvé" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Erreur interne du serveur" });
+    }
+});
+
+routes.post('/api/v1/livre', async (req, res) => {
+    const nouveauLivre = req.body;
+    try {
+        const livre = await BookModel.create(nouveauLivre);
+        res.status(201).json(livre);
+    } catch (error) {
+        res.status(500).json({ message: "Erreur interne du serveur" });
+    }
+});
+
+routes.put('/api/v1/livre/:idLivre', async (req, res) => {
+    const { idLivre } = req.params;
+    const donneesLivreModifie = req.body;
+    try {
+        const livreModifie = await BookModel.findByIdAndUpdate(idLivre, donneesLivreModifie, { new: true });
+        if (livreModifie) {
+            res.status(200).json(livreModifie);
+        } else {
+            res.status(404).json({ message: "Livre non trouvé" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Erreur interne du serveur" });
+    }
+});
+
+routes.delete('/api/v1/livre/:idLivre', async (req, res) => {
+    const { idLivre } = req.params;
+    try {
+        const livreSupprime = await BookModel.findByIdAndDelete(idLivre);
+        if (livreSupprime) {
+            res.status(200).json(livreSupprime);
+        } else {
+            res.status(404).json({ message: "Livre non trouvé" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Erreur interne du serveur" });
+    }
+});
+
+export default routes;
