3395a49499a5dec83263dd771d14f98a00de3101
rimoelharras <145035693+rimo1803@users.noreply.github.com>
2024-05-24 16:09:04 +0100

    modification

--- backend/api_livre/index.js
+++ backend/api_livre/index.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import livre from './routes/livreRoute.js';
-
+import cors from 'cors'
 const port = process.env.port;
 const url_mongoose = process.env.url_mongoose;
 const app = express();
 
 app.use(express.json());
-
+app.use(cors({origin : ['http://localhost:3000'],credentials : true}))
 mongoose
-  .connect(url_mongoose)
+  .connect('mongodb://127.0.0.1:27017/dbclients')
   .then(() => {
     console.log('Connected to mongo');
   })
@@ -17,9 +17,9 @@ mongoose
     console.log('Unable to connect to mongo : ' + err);
   });
 
-app.use('/livre/api/v1/livre', livre);
+app.use('/api/v1/livre', livre);
 
-app.listen(port, (err) => {
+app.listen( 3001, (err) => {
   if (err) console.log('Unable to start server :' + err);
   else console.log('Server started at ' + port);
 });
